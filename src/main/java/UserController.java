8051f5543cdeb7d7fafa87104e579d59aa3a5662
Zsofia Radai <zsofiaradai@gmail.com>
2017-09-21 22:55:18 +0200

    rendering template appears as plain text

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,12 +1,18 @@
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
-import static sun.plugin2.util.PojoUtil.toJson;
 
 public class UserController {
     public UserController(final UserService userService) {
+        //staticFiles.location("/public");
         port(9999);
+
+        get("/", (req, res) -> renderPage());
+
         get("/users", (req, res) -> userService.getAllUsers(), JsonUtil.json());
 
         after((req, res) -> {
@@ -25,4 +31,13 @@ public class UserController {
         }, JsonUtil.json());
     }
     // more routes
+
+    private static String renderPage(){
+        Map<String, Object> model = new HashMap<>();
+        return renderTemplate("velocity/index.vm", model);
+    }
+
+    private static String renderTemplate(String template, Map model) {
+        return new VelocityTemplateEngine().render(new ModelAndView(model, template));
+    }
 }
